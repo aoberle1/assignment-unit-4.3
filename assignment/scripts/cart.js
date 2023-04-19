49a7b3e97923a5a0f1e90bb8952dcf687fa4b7f0
Adam Oberle <oberle.adam@gmail.com>
2023-04-19 08:54:08 -0500

    created empty array for basket, started creating function for addItem

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,12 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+
+// 1.) Creating variable basket
+let basket = [];
+
+// 2.) Creating function addItem with parameter item
+function addItem ( item ){
+    
+}
